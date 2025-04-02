d0f1f8f669edaef23199099bdd8462c5932f7de2
ssstopeun <jeongji1416@gmail.com>
2025-04-02 15:32:16 +0900

    move properties

---
+++ client2/src/main/java/org/example/client1/ClientController.java
@@ -0,0 +1,28 @@
+package org.example.client1;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/config")
+public class ClientController {
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping
+    public void getConfig() {
+        log.info("App: "+appName + " (v" + appVersion + ")\n"+ " Message: "+message);
+    }
+}
